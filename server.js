43f6d261b627252c79d544386eb5f6083e322ba4
Ujjawal Aditya <nakshtra62033@gmail.com>
2025-08-15 13:39:43 +0530

    v1

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express=require("express");
 const extended = require("it/lib/extended");
 const app=express();
@@ -6,6 +7,9 @@ const mysql=require("mysql2")
 const bcrypt=require("bcrypt")
 const jwt = require("jsonwebtoken");
 const cookieParser=require("cookie-parser")
+const nodemailer = require('nodemailer');
+const cron=require("node-cron")
+const crypto=require("crypto")
 app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.urlencoded({extended:true}))
@@ -13,6 +17,59 @@ app.use(express.json());
 app.use(cookieParser())
 const JWT_SECRET="this is secret"
 const JWT_EXPIRES="1h"
+const mailer = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT || 587),
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
+
+mailer.verify((error, success) => {
+    if (error) {
+        console.error("SMTP Connection Error:", error);
+    } else {
+        console.log("SMTP Server is ready to send emails!");
+    }
+});
+async function sendEmail(to, subject, text) {
+  return mailer.sendMail({
+    from: process.env.FROM_EMAIL,
+    to,
+    subject,
+    text
+  });
+}
+cron.schedule("* * * * *",()=>{
+    const sql="select r.* ,u.email as user_email from reminders  r join users u on u.id=r.user_id where r.sent=0 and r.reminder_time<=NOW() "
+     db.query(sql,async(err,rows)=>{
+        if(err){
+            return res.json({
+                msg:"error fetching data from database"
+            })
+        }
+        for(const r of rows){
+            const msg=r.custom_message||`Reminder: "${r.contest_title}" is starting soon!\n${r.contest_url}`;
+             try{
+                 await sendEmail(r.user_email,"contest reminder",msg)
+                 db.query("update reminders set sent=1,sent_at=NOW() where id=?",[r.id])
+                 console.log(`Reminder ${r.id} sent to ${r.user_email}`);
+
+             }catch(e){
+                console.error(`Error sending email for reminder ${r.id}:`, e.message);
+
+               
+             }
+       
+        }
+     })
+
+})
+
+
+
 const logos = {
     "codeforces": "https://sta.codeforces.com/s/44350/images/codeforces-logo-with-telegram.png",
     "leetcode": "https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png",
@@ -50,6 +107,9 @@ function checkAuth(req, res, next) {
     next();
   });
 }
+function generateotp(){
+     return Math.floor(100000 + Math.random() * 900000).toString();
+}
 
 
 app.get("/register",(req,res)=>{
@@ -76,13 +136,13 @@ app.post("/login",async(req,res)=>{
         if(!isCorrect){
              return res.redirect("/login")
         }
-        const token=jwt.sign({id:user.id,email:user.email},JWT_SECRET,{expiresIn:JWT_EXPIRES})
+        const token=jwt.sign({id:user.id,email:user.email,isverified:user.is_verified},JWT_SECRET,{expiresIn:JWT_EXPIRES})
         res.cookie("token", token, {
         httpOnly: true, // Can't be accessed via JS
         secure: true,   // Only over HTTPS
          maxAge: 3600000 // 1 hour
 });
-res.json({ message: "Login successful" });
+return res.redirect("/dashboard")
 
         
     })
@@ -94,19 +154,31 @@ app.post("/register",async(req,res)=>{
     const {name,email,password}=req.body;
     const hashedpassword= await bcrypt.hash(password,10);
     
+    
     const registerquery="insert into users(name,email,password)values(?,?,?)"
     db.query(registerquery,[name,email,hashedpassword],(err,result)=>{
         if(err){
             return res.send(err)
         }
-        res.send("user Registered Sucessfully")
+      
+        res.redirect("/")
     })
 
 })
+app.post('/logout', (req, res) => {
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true,     
+        sameSite: 'Strict'
+    });
+    res.redirect('/login');
+});
+
 
 app.get("/",checkAuth,async(req,res)=>{
     const response=await fetch("https://competeapi.vercel.app/contests/upcoming/");
     let contests=await response.json()
+    console.log(contests)
     contests=contests.map((c)=>{
         return{ ...c,
         logo:logos[c.site],
@@ -114,11 +186,90 @@ app.get("/",checkAuth,async(req,res)=>{
     }
         
     })
-    // console.log(response)
     
     
-    return res.render("home",{contests})
+    
+    return res.render("home",{contests,user:req.user})
 })
+app.post("/setreminder", checkAuth,async(req,res)=>{
+    const {contestUrl,reminderTime,contestTitle,custoMessage}=req.body;
+    console.log(req.user.id)
+    
+    console.log(`contest Url:${contestUrl} and remainderTime is ${reminderTime} contestTitle is ${contestTitle}`)
+ const reminderquery="insert into  reminders(user_id,contest_title,contest_url,reminder_time, custom_message) values(?,?,?,?,?)"
+ db.query(reminderquery,[req.user.id,contestTitle,contestUrl,reminderTime,custoMessage],(err,result)=>{
+    if(err){
+        return res.json({msg:"Problem inserting data into database"})
+    }
+    return res.json({
+        msg:"sucessfully inserted into remainder"
+    })
+
+ })
+})
+app.get("/dashboard",checkAuth,async(req,res)=>{
+    const queryReminder="select * from reminders where user_id=?"
+    
+    db.query(queryReminder,[req.user.id],(err,result)=>{
+        if(err){
+            return res.json({
+                message:"Error findig contests"
+            })
+        }
+        console.log(result)
+        return res.render("dashboard",{user:req.user,
+            contests:result
+        })
+        
+    })
+    
+})
+app.post("/register/otp",checkAuth,async(req,res)=>{
+    const {email}=req.body;
+    console.log(email)
+    const otp=Math.floor(100000 + Math.random() * 900000).toString();
+    console.log(otp)
+      db.query("insert into  email_otps(email,otp,expires_at)values (?,?,?) ",[email,otp,new Date(Date.now() + 10 * 60 * 1000)],async(err,result)=>{
+               if(err){
+                 return res.json({msg:"problem sending otp"})
+               }
+               await sendEmail(email,"Email verification",`your otp is${otp}`)
+               res.render("verifyotp",{user:req.user})
+        })
+
+})
+app.post("/verify/otp", checkAuth, (req, res) => {
+  const { otp } = req.body; 
+
+  db.query(
+    "SELECT * FROM email_otps WHERE email=? AND otp=? AND expires_at > NOW()",
+    [req.user.email, otp],
+    (err, result) => {
+      if (err) return res.status(500).json({ msg: "Database error" });
+
+      if (result.length === 0) {
+        return res.status(400).json({ msg: "Invalid or expired OTP" });
+      }
+
+   
+      db.query(
+        "UPDATE users SET is_verified = 1 WHERE email = ?",
+        [req.user.email],
+        (err2, result2) => {
+          if (err2) return res.status(500).json({ msg: "Failed to verify user" });
+
+          
+          db.query("DELETE FROM email_otps WHERE email = ?", [req.user.email], (err3) => {
+            if (err3) console.log("Failed to delete OTP:", err3);
+           
+            return res.json({ msg: "Email verified successfully!" });
+          });
+        }
+      );
+    }
+  );
+});
+
 
 app.listen(3000,(req,res)=>{
     console.log("site is live at 3000")
